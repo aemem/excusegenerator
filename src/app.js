1f667055a0f54727953d9dc8c558fb4a5220e00c
aemem <anamoliner.marques@gmail.com>
2022-03-12 10:38:06 +0000

    primero

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("myExcuse").innerHTML = excuseGenerator();
+};
+
+const excuseGenerator = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed on", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let i = Math.floor(Math.random() * who.length);
+  let j = Math.floor(Math.random() * action.length);
+  let k = Math.floor(Math.random() * what.length);
+  let l = Math.floor(Math.random() * when.length);
+
+  let excuse = who[i] + " " + action[j] + " " + what[k] + " " + when[l];
+
+  return excuse;
 };
